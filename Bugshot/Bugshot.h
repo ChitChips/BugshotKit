6d8900f912b3fd626f9dbc08bbba6fe48cba2b27
Aurélien PINSTON <pinston.aurelien@gmail.com>
2016-02-26 15:51:37 +0100

    - header file

--- Bugshot/Bugshot.h
+++ Bugshot/Bugshot.h
@@ -17,3 +17,6 @@ FOUNDATION_EXPORT const unsigned char BugshotVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Bugshot/PublicHeader.h>
 
 
+#import <Bugshot/BSKMainViewController.h>
+#import <Bugshot/BSKWindow.h>
+#import <Bugshot/BugshotKit.h>
